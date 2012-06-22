bf895c4a58c2092bd418d990ad8647efc1fa1d87
Developer <developer@1qz.terra.com.br>
2012-06-22 20:13:24 -0300

    cicero lindo

--- benchmark-hillclimb.sh
+++ benchmark-hillclimb.sh
@@ -2,6 +2,6 @@
 
 for i in u159 u574 u1060 u1432 u1817 u2152 u2319; do
     echo hillclimb $i
-    python tsp.py -o results/hillclimb-${i}.png -n 100 -m reversed_sections -a hillclimb fonts/${i}.txt >> results/${i}.txt
+    python tsp.py -o results/hillclimb-${i}.png -n 10000 -m reversed_sections -a hillclimb fonts/${i}.txt >> results/${i}.txt
     sleep 0.1
 done
